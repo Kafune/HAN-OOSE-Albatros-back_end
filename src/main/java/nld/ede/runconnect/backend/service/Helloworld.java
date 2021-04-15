1d4af3e6c6c8f2a3a4bc197923665ba5bce972a7
Robert Boudewijn <RP.Boudewijn@student.han.nl>
2021-04-15 17:13:31 +0200

    Build server

---
+++ src/main/java/nld/ede/runconnect/backend/service/Helloworld.java
@@ -0,0 +1,25 @@
+package nld.ede.runconnect.backend.service;
+
+import nld.ede.runconnect.backend.service.dto.HelloworldDTO;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.Date;
+
+
+@Path("hello")
+public class Helloworld {
+    @GET
+    @Path("/")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response helloworld() {
+        HelloworldDTO helloworldDTO = new HelloworldDTO();
+        helloworldDTO.text= "hello world!";
+        var date = new Date();
+        helloworldDTO.time = String.valueOf(date.getTime());
+        return Response.status(200).entity(helloworldDTO).build();
+    }
+}
