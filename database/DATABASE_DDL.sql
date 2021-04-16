ccf832378dcd06f5d0f74342163e75badae10df9
Mohammad Yasin <M.Yasin@student.han.nl>
2021-04-16 13:21:05 +0200

    database-ddl engels

--- database/DATABASE_DDL.sql
+++ database/DATABASE_DDL.sql
@@ -1,92 +1,95 @@
 /*==============================================================*/
 /* DBMS name:      MySQL 5.0                                    */
-/* Created on:     16-4-2021 10:54:19                           */
+/* Created on:     16-4-2021 13:02:34                           */
 /*==============================================================*/
 
+drop table if exists ACTIVITY;
 
-drop table if exists ACTIVITEIT;
+drop table if exists COORDINATES;
 
-drop table if exists AFBEELDING;
+drop table if exists IMAGE;
 
-drop table if exists BEZIENSWAARDIGHEID;
-
-drop table if exists GEBRUIKER;
+drop table if exists POI;
 
 drop table if exists ROUTE;
 
+drop table if exists SEGMENT;
+
 drop table if exists SEGMENTINROUTE;
 
+drop table if exists `USER`;
+
 /*==============================================================*/
-/* Table: ACTIVITEIT                                            */
+/* Table: ACTIVITY                                              */
 /*==============================================================*/
-create table ACTIVITEIT
+create table ACTIVITY
 (
-   ACTIVITEITID         int not null,
+   ACTIVITYID           int not null auto_increment,
    ROUTEID              int not null,
-   GEBRUIKERID          int not null,
-   PUNTENWAARDE         int not null,
-   DURATIE              bigint,
+   USERID               int not null,
+   POINT                int not null,
+   DURATION             bigint,
    TEMPO                int,
-   AFGELEGDE_AFSTAND    int,
-   primary key (ACTIVITEITID)
+   DISTANCE             int,
+   primary key (ACTIVITYID)
 );
 
-
 /*==============================================================*/
-/* Table: AFBEELDING                                            */
+/* Table: COORDINATES                                           */
 /*==============================================================*/
-create table AFBEELDING
+create table COORDINATES
 (
-   GEBRUIKERID          int not null,
-   ACTIVITEITID         int,
-   BESTANDSNAAM         varchar(120) not null,
-   BESCHRIJVING         varchar(100),
-   unique key AK_KEY_1 (GEBRUIKERID)
+   COORDINATESID        int not null auto_increment,
+   LATITUDE             point not null,
+   LONGITUDE            point not null,
+   ALTITUDE             int not null,
+   primary key (COORDINATESID)
 );
- 
 
 /*==============================================================*/
-/* Table: BEZIENSWAARDIGHEID                                    */
+/* Table: IMAGE                                                 */
 /*==============================================================*/
-create table BEZIENSWAARDIGHEID
+create table IMAGE
+(
+   USERID               int not null,
+   ACTIVITYID           int,
+   FILENAME             varchar(120) not null,
+   DESCRIPTION          varchar(100),
+   unique key AK_KEY_1 (USERID)
+);
+
+/*==============================================================*/
+/* Table: POI                                                   */
+/*==============================================================*/
+create table POI
 (
    SEGMENTID            int not null,
-   NAAM                 varchar(100) not null,
-   ROUTEID              int,
-   BESCHRIJVING         varchar(100),
-   primary key (SEGMENTID, NAAM)
+   NAME                 varchar(100) not null,
+   DESCRIPTION          varchar(100),
+   primary key (SEGMENTID, NAME)
 );
- 
-
-/*==============================================================*/
-/* Table: GEBRUIKER                                             */
-/*==============================================================*/
-create table GEBRUIKER
-(
-   GEBRUIKERID          int not null,
-   VOORNAAM             varchar(60) not null,
-   ACHTERNAAM           varchar(60) not null,
-   GEBRUIKERSNAAM       varchar(20) not null,
-   WACHTWOORD           varchar(130) not null,
-   GEBOORTEDATUM        date not null,
-   TOTAALSCORE          int not null,
-   primary key (GEBRUIKERID)
-);
- 
 
 /*==============================================================*/
 /* Table: ROUTE                                                 */
 /*==============================================================*/
 create table ROUTE
 (
-   ROUTEID              int not null,
-   HOOGTEVERSCHIL       int not null,
-   STARTCOORD           numeric(12,9) not null,
-   EINDCOORD            numeric(12,9) not null,
+   ROUTEID              int not null auto_increment,
    AFSTAND              int not null,
    primary key (ROUTEID)
 );
- 
+
+/*==============================================================*/
+/* Table: SEGMENT                                               */
+/*==============================================================*/
+create table SEGMENT
+(
+   SEGMENTID            int not null auto_increment,
+   STARTCOORD           int not null,
+   ENDCOORD             int not null,
+   PREVIOUSSEGMENT      int,
+   primary key (SEGMENTID)
+);
 
 /*==============================================================*/
 /* Table: SEGMENTINROUTE                                        */
@@ -95,28 +98,50 @@ create table SEGMENTINROUTE
 (
    ROUTEID              int not null,
    SEGMENTID            int not null,
-   STARTCOORD           point not null,
-   ENDCOORD             point not null,
-   VOORGAANDSEGMENT     int,
    primary key (ROUTEID, SEGMENTID)
 );
- 
 
-alter table ACTIVITEIT add constraint FK_REFERENCE_1 foreign key (GEBRUIKERID)
-      references GEBRUIKER (GEBRUIKERID);
+/*==============================================================*/
+/* Table: USER                                                  */
+/*==============================================================*/
+create table `USER`
+(
+   USERID               int not null auto_increment,
+   FIRSTNAME            varchar(60) not null,
+   LASTNAME             varchar(60) not null,
+   E_MAILADRES          varchar(100) not null,
+   USERNAME             varchar(20) not null,
+   PASSWORD             varchar(130) not null,
+   BIRTHDATE            date not null,
+   TOTALSCORE           int not null default 0,
+   primary key (USERID),
+   unique key AK_KEY_2 (E_MAILADRES),
+   unique key AK_KEY_3 (USERNAME)
+);
 
-alter table ACTIVITEIT add constraint FK_REFERENCE_2 foreign key (ROUTEID)
-      references ROUTE (ROUTEID);
+alter table ACTIVITY add constraint FK_REFERENCE_1 foreign key (USERID)
+      references `USER` (USERID) on delete cascade on update cascade;
 
-alter table AFBEELDING add constraint FK_REFERENCE_3 foreign key (ACTIVITEITID)
-      references ACTIVITEIT (ACTIVITEITID);
+alter table ACTIVITY add constraint FK_REFERENCE_2 foreign key (ROUTEID)
+      references ROUTE (ROUTEID) on delete cascade on update cascade;
 
-alter table AFBEELDING add constraint FK_REFERENCE_6 foreign key (GEBRUIKERID)
-      references GEBRUIKER (GEBRUIKERID);
+alter table IMAGE add constraint FK_REFERENCE_3 foreign key (ACTIVITYID)
+      references ACTIVITY (ACTIVITYID) on delete cascade on update cascade;
 
-alter table BEZIENSWAARDIGHEID add constraint FK_REFERENCE_7 foreign key (ROUTEID, SEGMENTID)
-      references SEGMENTINROUTE (ROUTEID, SEGMENTID);
+alter table IMAGE add constraint FK_REFERENCE_6 foreign key (USERID)
+      references `USER` (USERID) on delete cascade on update cascade;
+
+alter table POI add constraint FK_REFERENCE_8 foreign key (SEGMENTID)
+      references SEGMENT (SEGMENTID) on delete cascade on update cascade;
+
+alter table SEGMENT add constraint FK_REFERENCE_10 foreign key (ENDCOORD)
+      references COORDINATES (COORDINATESID) on delete cascade on update cascade;
+
+alter table SEGMENT add constraint FK_REFERENCE_9 foreign key (STARTCOORD)
+      references COORDINATES (COORDINATESID) on delete cascade on update cascade;
 
 alter table SEGMENTINROUTE add constraint FK_REFERENCE_4 foreign key (ROUTEID)
-      references ROUTE (ROUTEID);
+      references ROUTE (ROUTEID) on delete cascade on update cascade;
 
+alter table SEGMENTINROUTE add constraint FK_REFERENCE_7 foreign key (SEGMENTID)
+      references SEGMENT (SEGMENTID) on delete cascade on update cascade;
