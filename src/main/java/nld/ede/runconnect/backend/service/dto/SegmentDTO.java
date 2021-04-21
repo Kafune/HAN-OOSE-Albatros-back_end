05eb5cf066d40289c1f558ee891a2748aecba83b
Robert Boudewijn <RP.boudewijn@student.han.nl>
2021-04-21 11:24:43 +0200

    JSON to RouteDTO works

--- src/main/java/nld/ede/runconnect/backend/service/dto/SegmentDTO.java
+++ src/main/java/nld/ede/runconnect/backend/service/dto/SegmentDTO.java
@@ -2,6 +2,6 @@ package nld.ede.runconnect.backend.service.dto;
 
 public class SegmentDTO {
         public int id;
-        public CoordinateDTO startCoordinates;
-        public CoordinateDTO endCoordinates;
+        public CoordinateDTO startCoordinate;
+        public CoordinateDTO endCoordinate;
 }
