39efcaae07ba95bcfa578a1807cf22073036f1fa
Mohammad Yasin <M.Yasin@student.han.nl>
2021-04-22 00:10:29 +0200

    connection met database gemaakt

--- src/main/java/nld/ede/runconnect/backend/dao/RouteDAO.java
+++ src/main/java/nld/ede/runconnect/backend/dao/RouteDAO.java
@@ -1,5 +1,6 @@
 package nld.ede.runconnect.backend.dao;
 
+import nld.ede.runconnect.backend.domain.Coordinate;
 import nld.ede.runconnect.backend.domain.Route;
 import nld.ede.runconnect.backend.domain.Segment;
 
@@ -41,20 +42,47 @@ public class RouteDAO implements IRouteDAO {
 
     private Route extractRoute(ResultSet resultSet) throws SQLException {
         int routeId = -1;
-        if (routeId != resultSet.getInt(1)) {
-            Route route = new Route();
+        List<Segment> segmentList = new ArrayList<>();
+        Route route;
+        if (routeId == resultSet.getInt(1)) {
+            segmentList.add(extractSegment(resultSet));
+        }
+        else {
+            route = new Route();
             routeId = resultSet.getInt(1);
             route.setRouteId(routeId);
             route.setDistance(resultSet.getInt(2));
             route.setName(resultSet.getString(3));
-            route.setSegments(extractSegment(resultSet));
+            segmentList.add(extractSegment(resultSet));
         }
+        assert route != null;
+        route.setSegments(segmentList);
 
         return null;
     }
 
-    private List<Segment> extractSegment(ResultSet resultSet) {
-        return null;
+    private Segment extractSegment(ResultSet resultSet) throws SQLException {
+        Segment segment = new Segment();
+        segment.setId(resultSet.getInt(4));
+        segment.setStartCoordinate(extractStartCoordinate(resultSet));
+        segment.setEndCoordinate(extractEndCoordinate(resultSet));
+        return segment;
+    }
+
+    private Coordinate extractEndCoordinate(ResultSet resultSet) throws SQLException {
+        Coordinate coordinate = new Coordinate();
+        coordinate.setAltitude(resultSet.getInt(9));
+        coordinate.setLongitude(resultSet.getDouble(10));
+        coordinate.setLatitude(resultSet.getDouble(11));
+        return coordinate;
+    }
+
+    private Coordinate extractStartCoordinate(ResultSet resultSet) throws SQLException {
+        Coordinate coordinate = new Coordinate();
+        coordinate.setAltitude(resultSet.getInt(6));
+        coordinate.setLongitude(resultSet.getDouble(7));
+        coordinate.setLatitude(resultSet.getDouble(8));
+        return coordinate;
     }
 
     public String findAllRoutesStatement() {
