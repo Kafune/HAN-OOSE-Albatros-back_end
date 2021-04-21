daf107c67ea7957d6d2e61444e21f485aaa69801
Robert Boudewijn <RP.boudewijn@student.han.nl>
2021-04-21 11:36:50 +0200

    Update DATABASE_DDL.sql

--- database/DATABASE_DDL.sql
+++ database/DATABASE_DDL.sql
@@ -3,36 +3,35 @@
 /* Created on:     19-4-2021 15:14:38                           */
 /*==============================================================*/
 
-
-drop table if exists ACTIVITY;
-
-drop table if exists COORDINATES;
-
-drop table if exists IMAGE;
-
 drop table if exists POI;
-
-drop table if exists ROUTE;
-
-drop table if exists SEGMENT;
 
 drop table if exists SEGMENTINROUTE;
 
+drop table if exists COORDINATES;
+
+drop table if exists SEGMENT;
+
+drop table if exists ACTIVITY;
+
+drop table if exists IMAGE;
+
 drop table if exists `USER`;
+
+drop table if exists ROUTE;
 
 /*==============================================================*/
 /* Table: ACTIVITY                                              */
 /*==============================================================*/
 create table ACTIVITY
 (
-   ACTIVITYID           int not null auto_increment,
-   ROUTEID              int not null,
-   USERID               int not null,
-   POINT                int not null,
-   DURATION             bigint,
-   TEMPO                int,
-   DISTANCE             int,
-   primary key (ACTIVITYID)
+    ACTIVITYID           int not null auto_increment,
+    ROUTEID              int not null,
+    USERID               int not null,
+    POINT                int not null,
+    DURATION             bigint,
+    TEMPO                int,
+    DISTANCE             int,
+    primary key (ACTIVITYID)
 );
 
 /*==============================================================*/
@@ -40,10 +39,10 @@ create table ACTIVITY
 /*==============================================================*/
 create table COORDINATES
 (
-   COORDINATESID        int not null auto_increment,
-   LOCATION             point not null,
-   ALTITUDE             int not null,
-   primary key (COORDINATESID)
+    COORDINATESID        int not null auto_increment,
+    LOCATION             point not null,
+    ALTITUDE             int not null,
+    primary key (COORDINATESID)
 );
 
 /*==============================================================*/
@@ -51,10 +50,10 @@ create table COORDINATES
 /*==============================================================*/
 create table IMAGE
 (
-   USERID               int not null,
-   ACTIVITYID           int,
-   FILENAME             varchar(120) not null,
-   DESCRIPTION          varchar(100)
+    USERID               int not null,
+    ACTIVITYID           int,
+    FILENAME             varchar(120) not null,
+    DESCRIPTION          varchar(100)
 );
 
 /*==============================================================*/
@@ -62,10 +61,10 @@ create table IMAGE
 /*==============================================================*/
 create table POI
 (
-   SEGMENTID            int not null,
-   NAME                 varchar(100) not null,
-   DESCRIPTION          varchar(100),
-   primary key (SEGMENTID, NAME)
+    SEGMENTID            int not null,
+    NAME                 varchar(100) not null,
+    DESCRIPTION          varchar(100),
+    primary key (SEGMENTID, NAME)
 );
 
 /*==============================================================*/
@@ -73,10 +72,10 @@ create table POI
 /*==============================================================*/
 create table ROUTE
 (
-   ROUTEID              int not null auto_increment,
-   NAME                 VARCHAR(150) not null,
-   DISTANCE             int not null,
-   primary key (ROUTEID)
+    ROUTEID              int not null auto_increment,
+    NAME                 VARCHAR(150) not null,
+    DISTANCE             int not null,
+    primary key (ROUTEID)
 );
 
 /*==============================================================*/
@@ -84,10 +83,10 @@ create table ROUTE
 /*==============================================================*/
 create table SEGMENT
 (
-   SEGMENTID            int not null auto_increment,
-   STARTCOORD           int not null,
-   ENDCOORD             int not null,
-   primary key (SEGMENTID)
+    SEGMENTID            int not null auto_increment,
+    STARTCOORD           int not null,
+    ENDCOORD             int not null,
+    primary key (SEGMENTID)
 );
 
 /*==============================================================*/
@@ -95,10 +94,10 @@ create table SEGMENT
 /*==============================================================*/
 create table SEGMENTINROUTE
 (
-   ROUTEID              int not null,
-   SEGMENTID            int not null,
-   SEQUENCENR           int not null,
-   primary key (ROUTEID, SEGMENTID)
+    ROUTEID              int not null,
+    SEGMENTID            int not null,
+    SEQUENCENR           int not null,
+    primary key (ROUTEID, SEGMENTID)
 );
 
 /*==============================================================*/
@@ -106,43 +105,43 @@ create table SEGMENTINROUTE
 /*==============================================================*/
 create table `USER`
 (
-   USERID               int not null auto_increment,
-   FIRSTNAME            varchar(60) not null,
-   LASTNAME             varchar(60) not null,
-   E_MAILADRES          varchar(100) not null,
-   USERNAME             varchar(20) not null,
-   PASSWORD             varchar(130) not null,
-   BIRTHDATE            date not null,
-   TOTALSCORE           int not null default 0,
-   primary key (USERID),
-   unique key AK_KEY_2 (E_MAILADRES),
-   unique key AK_KEY_3 (USERNAME)
+    USERID               int not null auto_increment,
+    FIRSTNAME            varchar(60) not null,
+    LASTNAME             varchar(60) not null,
+    E_MAILADRES          varchar(100) not null,
+    USERNAME             varchar(20) not null,
+    PASSWORD             varchar(130) not null,
+    BIRTHDATE            date not null,
+    TOTALSCORE           int not null default 0,
+    primary key (USERID),
+    unique key AK_KEY_2 (E_MAILADRES),
+    unique key AK_KEY_3 (USERNAME)
 );
 
 alter table ACTIVITY add constraint FK_REFERENCE_1 foreign key (USERID)
-      references `USER` (USERID) on delete cascade on update cascade;
+    references `USER` (USERID) on delete cascade on update cascade;
 
 alter table ACTIVITY add constraint FK_REFERENCE_2 foreign key (ROUTEID)
-      references ROUTE (ROUTEID) on delete cascade on update cascade;
+    references ROUTE (ROUTEID) on delete cascade on update cascade;
 
 alter table IMAGE add constraint FK_REFERENCE_3 foreign key (ACTIVITYID)
-      references ACTIVITY (ACTIVITYID) on delete cascade on update cascade;
+    references ACTIVITY (ACTIVITYID) on delete cascade on update cascade;
 
 alter table IMAGE add constraint FK_REFERENCE_6 foreign key (USERID)
-      references `USER` (USERID) on delete cascade on update cascade;
+    references `USER` (USERID) on delete cascade on update cascade;
 
 alter table POI add constraint FK_REFERENCE_8 foreign key (SEGMENTID)
-      references SEGMENT (SEGMENTID) on delete cascade on update cascade;
+    references SEGMENT (SEGMENTID) on delete cascade on update cascade;
 
 alter table SEGMENT add constraint FK_REFERENCE_10 foreign key (ENDCOORD)
-      references COORDINATES (COORDINATESID) on delete cascade on update cascade;
+    references COORDINATES (COORDINATESID) on delete cascade on update cascade;
 
 alter table SEGMENT add constraint FK_REFERENCE_9 foreign key (STARTCOORD)
-      references COORDINATES (COORDINATESID) on delete cascade on update cascade;
+    references COORDINATES (COORDINATESID) on delete cascade on update cascade;
 
 alter table SEGMENTINROUTE add constraint FK_REFERENCE_4 foreign key (ROUTEID)
-      references ROUTE (ROUTEID) on delete cascade on update cascade;
+    references ROUTE (ROUTEID) on delete cascade on update cascade;
 
 alter table SEGMENTINROUTE add constraint FK_REFERENCE_7 foreign key (SEGMENTID)
-      references SEGMENT (SEGMENTID) on delete cascade on update cascade;
+    references SEGMENT (SEGMENTID) on delete cascade on update cascade;
 
