87a794bfcbe8134bb2ea1588309d24e9484c30b8
Mohammad Yasin <M.Yasin@student.han.nl>
2021-04-21 17:56:29 +0200

    converter aangepast

--- src/main/java/nld/ede/runconnect/backend/service/dto/DTOconverter.java
+++ src/main/java/nld/ede/runconnect/backend/service/dto/DTOconverter.java
@@ -22,7 +22,7 @@ public class DTOconverter {
 
         // Maak een lijst of segmentDTO for deze route
         List<SegmentDTO> segmentDTOList = new ArrayList<>();
-        domainToSegmentDTO(route);
+        segmentDTOList.add(domainToSegmentDTO(route));
         RouteDTO routeDTO = new RouteDTO();
         routeDTO.routeId = route.getRouteId();
         routeDTO.distance = route.getDistance();
@@ -32,28 +32,38 @@ public class DTOconverter {
 
     }
 
-    private static void domainToSegmentDTO(Route route) {
+    private static SegmentDTO domainToSegmentDTO(Route route) {
+        SegmentDTO segmentDTO = new SegmentDTO();
+
         for (Segment item : route.getSegments()) {
 
-            SegmentDTO segmentDTO = new SegmentDTO();
             segmentDTO.id = item.getId();
-
-            //Start coordinate of segment
-            CoordinateDTO startCoordinateDTO = new CoordinateDTO();
-
-            startCoordinateDTO.altitude = item.getStartCoordinate().getAltitude();
-            startCoordinateDTO.latitude = item.getStartCoordinate().getLatitude();
-            startCoordinateDTO.longitude = item.getStartCoordinate().getLongitude();
-            segmentDTO.startCoordinate = startCoordinateDTO;
-
-            //End coordinate of segment
-            CoordinateDTO endCoordinateDTO = new CoordinateDTO();
-
-            startCoordinateDTO.altitude = item.getEndCoordinate().getAltitude();
-            startCoordinateDTO.latitude = item.getEndCoordinate().getLatitude();
-            startCoordinateDTO.longitude = item.getEndCoordinate().getLongitude();
-            segmentDTO.endCoordinate = endCoordinateDTO;
+            segmentDTO.startCoordinate = domainToStartCoordinateDTO(item);
+            segmentDTO.endCoordinate = domainToEndCoordinateDTO(item);
         }
+        return segmentDTO;
+    }
+
+    private static CoordinateDTO domainToEndCoordinateDTO(Segment item) {
+        //End coordinate of segment
+        CoordinateDTO endCoordinateDTO = new CoordinateDTO();
+
+        endCoordinateDTO.altitude = item.getEndCoordinate().getAltitude();
+        endCoordinateDTO.latitude = item.getEndCoordinate().getLatitude();
+        endCoordinateDTO.longitude = item.getEndCoordinate().getLongitude();
+
+        return endCoordinateDTO;
+    }
+
+    private static CoordinateDTO domainToStartCoordinateDTO(Segment item) {
+        //Start coordinate of segment
+        CoordinateDTO startCoordinateDTO = new CoordinateDTO();
+
+        startCoordinateDTO.altitude = item.getStartCoordinate().getAltitude();
+        startCoordinateDTO.latitude = item.getStartCoordinate().getLatitude();
+        startCoordinateDTO.longitude = item.getStartCoordinate().getLongitude();
+
+        return startCoordinateDTO;
     }
 
 }
