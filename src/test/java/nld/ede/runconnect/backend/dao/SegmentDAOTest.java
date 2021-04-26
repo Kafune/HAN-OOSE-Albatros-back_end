bd28838bce6eca669fbaa155bd788de106c8eea3
Mohammad Yasin <M.Yasin@student.han.nl>
2021-04-26 10:15:47 +0200

    onnodige import weggehaald

--- src/test/java/nld/ede/runconnect/backend/dao/SegmentDAOTest.java
+++ src/test/java/nld/ede/runconnect/backend/dao/SegmentDAOTest.java
@@ -1,6 +1,5 @@
 package nld.ede.runconnect.backend.dao;
 
-import nld.ede.runconnect.backend.domain.Route;
 import nld.ede.runconnect.backend.domain.Segment;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
