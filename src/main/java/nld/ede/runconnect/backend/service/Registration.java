04ffc8e37a29ebd2b2ba799e8e39e3c065e1f402
Mohammad Yasin <M.Yasin@student.han.nl>
2021-05-10 10:13:23 +0200

    remote facade en DAO class

---
+++ src/main/java/nld/ede/runconnect/backend/service/Registration.java
@@ -0,0 +1,19 @@
+package nld.ede.runconnect.backend.service;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+@Path("registration")
+public class Registration {
+
+
+
+    @POST
+    public Response registerUser(){
+
+
+        return null;
+    }
+
+}
