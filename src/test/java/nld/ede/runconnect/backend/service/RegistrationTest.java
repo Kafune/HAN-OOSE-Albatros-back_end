c3a8f0392057c0d91dbdf88caad1db96292e1a62
Mohammad Yasin <M.Yasin@student.han.nl>
2021-05-10 14:15:01 +0200

    Registration.java 100 % procent test coverage

--- src/test/java/nld/ede/runconnect/backend/service/RegistrationTest.java
+++ src/test/java/nld/ede/runconnect/backend/service/RegistrationTest.java
@@ -1,6 +1,72 @@
 package nld.ede.runconnect.backend.service;
 
+import nld.ede.runconnect.backend.dao.RegistrationDAO;
+import nld.ede.runconnect.backend.domain.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.ws.rs.core.Response;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.*;
+
 public class RegistrationTest {
 
+    private Registration sut;
+    private RegistrationDAO registrationDAOMock;
 
+    @BeforeEach
+    public void setup() {
+        sut = new Registration();
+        registrationDAOMock = mock(RegistrationDAO.class);
+    }
+
+    @Test
+    public void registerUserCallsMethodInDAOTest() {
+        User user = new User();
+        try {
+            when(registrationDAOMock.registerUser(user)).thenReturn(true);
+            sut.setRegistrationDAO(registrationDAOMock);
+
+            sut.registerUser(user);
+
+            verify(registrationDAOMock).registerUser(user);
+        } catch (SQLException e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void registerUserReturns201IfUserRegistered() {
+        User user = new User();
+        try {
+            when(registrationDAOMock.registerUser(user)).thenReturn(true);
+            sut.setRegistrationDAO(registrationDAOMock);
+
+            Response response = sut.registerUser(user);
+            int actualResponseStatus = response.getStatus();
+
+            assertEquals(201, actualResponseStatus);
+        } catch (SQLException e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void registerUserReturns200IfUserFoundInDatabase() {
+        User user = new User();
+        try {
+            when(registrationDAOMock.registerUser(user)).thenReturn(false);
+            sut.setRegistrationDAO(registrationDAOMock);
+
+            Response response = sut.registerUser(user);
+            int actualResponseStatus = response.getStatus();
+
+            assertEquals(200, actualResponseStatus);
+        } catch (SQLException e) {
+            fail();
+        }
+    }
 }
