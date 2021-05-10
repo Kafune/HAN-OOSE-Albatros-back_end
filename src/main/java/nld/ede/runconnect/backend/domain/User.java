4750c4b3e7d21201c03642478671637bfc1cffc9
Mohammad Yasin <M.Yasin@student.han.nl>
2021-05-10 10:42:22 +0200

    domain user toegevoegd

---
+++ src/main/java/nld/ede/runconnect/backend/domain/User.java
@@ -0,0 +1,12 @@
+package nld.ede.runconnect.backend.domain;
+
+public class User {
+
+    private int userId;
+    private String firstname;
+    private String lastname;
+    private String emailAddress;
+    private String username;
+    private String birthdate;
+    private int totalScore;
+}
