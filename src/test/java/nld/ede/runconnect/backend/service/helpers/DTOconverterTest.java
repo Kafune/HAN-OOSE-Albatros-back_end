6982f17f5c3f88533f25fa7ba573eab81610fdc6
Mohammad Yasin <M.Yasin@student.han.nl>
2021-05-20 17:21:31 +0200

    naam naam test class veranderd

--- src/test/java/nld/ede/runconnect/backend/service/helpers/DTOconverterTest.java
+++ src/test/java/nld/ede/runconnect/backend/service/helpers/DTOconverterTest.java
@@ -12,7 +12,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 
-class DTConverterTest {
+class DTOconverterTest {
     private DTOconverter dtoconverter;
 
     @BeforeEach
