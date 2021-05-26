392e3edb7a4822ce5bba8b5a1f19dd726980f477
nehlis <nielsbosman@mail.com>
2021-05-26 14:32:37 +0200

    Add method with headers and it's comments.

--- src/main/java/nld/ede/runconnect/backend/dao/IUserDAO.java
+++ src/main/java/nld/ede/runconnect/backend/dao/IUserDAO.java
@@ -1,5 +1,17 @@
 package nld.ede.runconnect.backend.dao;
 
+import nld.ede.runconnect.backend.domain.User;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public interface IUserDAO
 {
+    /**
+     * Searches for users by search value.
+     * @param searchValue The value to search for.
+     * @return The found users, if any.
+     * @throws SQLException Exception if SQL fails.
+     */
+    ArrayList<User> searchForUsers(String searchValue) throws SQLException;
 }
--- src/main/java/nld/ede/runconnect/backend/dao/UserDAO.java
+++ src/main/java/nld/ede/runconnect/backend/dao/UserDAO.java
@@ -1,5 +1,23 @@
 package nld.ede.runconnect.backend.dao;
 
-public class UserDAO
+import nld.ede.runconnect.backend.domain.User;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class UserDAO implements IUserDAO
 {
+    private final String SEARCH_USERS_QUERY = "";
+
+    /**
+     * Searches for users by search value.
+     * @param searchValue The value to search for.
+     * @return The found users, if any.
+     * @throws SQLException Exception if SQL fails.
+     */
+    @Override
+    public ArrayList<User> searchForUsers(String searchValue) throws SQLException
+    {
+        return null;
+    }
 }
