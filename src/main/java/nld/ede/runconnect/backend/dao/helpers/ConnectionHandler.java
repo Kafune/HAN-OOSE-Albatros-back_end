c8bcbb1fd893e6e2b73c936c039b7905d6a5fd64
Patrick Roelofs <PJL.Roelofs@student.han.nl>
2021-05-26 17:58:41 +0200

    add exception to ConnectionHandler

--- src/main/java/nld/ede/runconnect/backend/dao/helpers/ConnectionHandler.java
+++ src/main/java/nld/ede/runconnect/backend/dao/helpers/ConnectionHandler.java
@@ -5,20 +5,20 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class ConnectionHandler {
-    public static void close(PreparedStatement preparedStatement, ResultSet resultSet) {
+    public static void close(PreparedStatement preparedStatement, ResultSet resultSet) throws SQLException {
         if(resultSet != null) {
             try {
                 resultSet.close();
-            } catch (SQLException e) {
-                // logger
+            } catch (SQLException exception) {
+                throw exception;
             }
         }
 
         if(preparedStatement != null) {
             try {
                 preparedStatement.close();
-            } catch (SQLException e) {
-                // logger
+            } catch (SQLException exception) {
+                throw exception;
             }
         }
     }
