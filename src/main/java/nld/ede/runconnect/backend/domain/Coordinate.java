c5d3374abbc02e230e366233d7d941a6479c9955
Patrick Roelofs <PJL.Roelofs@student.han.nl>
2021-05-27 11:28:11 +0200

    typo

--- src/main/java/nld/ede/runconnect/backend/domain/Coordinate.java
+++ src/main/java/nld/ede/runconnect/backend/domain/Coordinate.java
@@ -1,7 +1,7 @@
 package nld.ede.runconnect.backend.domain;
 
 public class Coordinate {
-    //Chose for double because a float is Sufficient for storing 6 to 7 decimal digits. While a coordinate is max 13chars (https://stackoverflow.com/questions/15965166/what-are-the-lengths-of-location-coordinates-latitude-and-longitude#answer-61632275)
+    //Chosen for double because a float is Sufficient for storing 6 to 7 decimal digits. While a coordinate is max 13chars (https://stackoverflow.com/questions/15965166/what-are-the-lengths-of-location-coordinates-latitude-and-longitude#answer-61632275)
     private double longitude;
     private double latitude;
     private float altitude;
