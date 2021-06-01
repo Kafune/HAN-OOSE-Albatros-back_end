26a9a50f8de9b2dfde16a783e411017e8ebdce5a
Niels Bosman <NR.Bosman@student.han.nl>
2021-06-01 16:09:36 +0200

    Adjust user firewall.

--- src/main/java/nld/ede/runconnect/backend/service/filters/firewall/FirewallUsers.java
+++ src/main/java/nld/ede/runconnect/backend/service/filters/firewall/FirewallUsers.java
@@ -2,15 +2,14 @@ package nld.ede.runconnect.backend.service.filters.firewall;
 
 import nld.ede.runconnect.backend.service.tokens.TokenHashMap;
 
+import javax.ws.rs.Path;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.PathSegment;
 import javax.ws.rs.core.Response;
 import java.util.List;
 
-public class FirewallUsers  implements IFirewall{
-
-
+public class FirewallUsers implements IFirewall {
     @Override
     public void rules(ContainerRequestContext requestContext, List<PathSegment> pathSegments, MultivaluedMap<String, String> parameters) {
         String token = parameters.getFirst("token");
@@ -18,13 +17,25 @@ public class FirewallUsers  implements IFirewall{
 
         //Path("/")
         //registerUser
-        if(requestContext.getMethod().equals("POST") && pathSegments.size() <=1){
+        if (requestContext.getMethod().equals("POST") && pathSegments.size() <= 2) {
             return;
         }
 
-        //Path("/find/{search-value}")
-        //searchForUser
-        if(pathSegments.get(1).toString().equals("find") && tokenHashMap.doesExist(token)){
+        // Path("/find/{search-value}")
+        // searchForUser
+        if (pathSegments.get(1).toString().equals("find") && tokenHashMap.doesExist(token)) {
+            return;
+        }
+
+        // Path("/{follower-id}/unfollows/{followee-id}")
+        // Unfollow
+        if (pathSegments.get(2).toString().equals("unfollows") && requestContext.getMethod().equals("DELETE") && tokenHashMap.doesExist(token)) {
+            return;
+        }
+
+        // Path("/{follower-id}/follows/{followee-id}")
+        // Unfollow
+        if (pathSegments.get(2).toString().equals("follows") && requestContext.getMethod().equals("POST") && tokenHashMap.doesExist(token)) {
             return;
         }
 
