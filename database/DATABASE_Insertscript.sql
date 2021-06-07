5cbe816fea0f7089bb86d63e235f50450dc5ea32
Patrick Roelofs <PJL.Roelofs@student.han.nl>
2021-06-07 13:48:21 +0200

    add insertscript

---
+++ database/DATABASE_Insertscript.sql
@@ -0,0 +1,14 @@
+INSERT INTO run_connect.user (USERID, FIRSTNAME, LASTNAME, E_MAILADRES, USERNAME, TOTALSCORE, IMAGE_URL, ADMIN) VALUES (15, 'Patrick', 'Roelofs', 'email0@email.com', 'Patrick Roelofs', 40, 'https://lh3.googleusercontent.com/a-/AOh14GjXxHNGQleDNo2BdpFZ2-uJuSbDQlrXrnxXN5MjHaA=s96-c', 0);
+INSERT INTO run_connect.user (USERID, FIRSTNAME, LASTNAME, E_MAILADRES, USERNAME, TOTALSCORE, IMAGE_URL, ADMIN) VALUES (34, 'Robert', 'Boudewijn', 'RB@email.com', 'Robert Boudewijn', 420, 'https://lh3.googleusercontent.com/a-/AOh14GgTX_lxhop3SzZtZmdPv1UBaKR7ZjuJEnAPrz-J=s96-c', 0);
+INSERT INTO run_connect.user (USERID, FIRSTNAME, LASTNAME, E_MAILADRES, USERNAME, TOTALSCORE, IMAGE_URL, ADMIN) VALUES (66, 'Mohammad', 'Yasin', 'MY@email.com', 'Mohammad Yasin', 357, 'https://lh3.googleusercontent.com/a-/AOh14GjlqSIuJB3FVXY9kBMrPq10mQaYkrCqHHymHp-tYA=s96-c', 0);
+INSERT INTO run_connect.user (USERID, FIRSTNAME, LASTNAME, E_MAILADRES, USERNAME, TOTALSCORE, IMAGE_URL, ADMIN) VALUES (75, 'Kachung', 'Li', 'KL@email.com', 'Kachung Li', 5, 'https://lh3.googleusercontent.com/ogw/ADea4I5joTMvwREIJJORnKazk3L7ymAQ_WR_uv_4NFft=s83-c-mo', 0);
+INSERT INTO run_connect.user (USERID, FIRSTNAME, LASTNAME, E_MAILADRES, USERNAME, TOTALSCORE, IMAGE_URL, ADMIN) VALUES (12, 'Niels', 'Bosman', 'NB@email.com', 'Niels Bosman', 68, 'https://lh3.googleusercontent.com/ogw/ADea4I6SUuoqKWnu1VsRHYuM36rQUaElVcdZxUprTQpvhg=s83-c-mo', 0);
+
+
+INSERT INTO run_connect.activity (USERID, POINT, DURATION, DISTANCE, ROUTEID, DATE) VALUES (15, 10, 250178, 2.55, 1, '2021-05-24 13:40:06');
+INSERT INTO run_connect.activity (USERID, POINT, DURATION, DISTANCE, ROUTEID, DATE) VALUES (34, 50, 1550178, 5.55, 1, '2021-05-26 13:40:07');
+INSERT INTO run_connect.activity (USERID, POINT, DURATION, DISTANCE, ROUTEID, DATE) VALUES (34, 20, 450178, 4.55, 1, '2021-05-14 13:40:08');
+INSERT INTO run_connect.activity (USERID, POINT, DURATION, DISTANCE, ROUTEID, DATE) VALUES (34, 30, 850178, 2.55, 1, '2021-05-16 13:40:09');
+INSERT INTO run_connect.activity (USERID, POINT, DURATION, DISTANCE, ROUTEID, DATE) VALUES (66, 60, 250178, 7.55, 1, '2021-05-19 13:40:10');
+INSERT INTO run_connect.activity (USERID, POINT, DURATION, DISTANCE, ROUTEID, DATE) VALUES (75, 40, 350178, 1.55, 1, '2021-05-21 13:40:11');
+INSERT INTO run_connect.activity (USERID, POINT, DURATION, DISTANCE, ROUTEID, DATE) VALUES (12, 100, 650355, 4.55, 1, '2021-05-21 13:40:12');
